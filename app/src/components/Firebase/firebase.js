116f4932fb0c3f2ea1579eb2abf2ae2705befcac
Summer 19 <summer19@iMac-3.local>
2019-06-21 17:09:04 -0700

    react

--- app/src/components/Firebase/firebase.js
+++ app/src/components/Firebase/firebase.js
@@ -1,3 +1,6 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
 const config = {
     apiKey: "AIzaSyAE3rMblfzZOFTXxSUHezi3jquVf0bXBhY",
     authDomain: "propellor-navigation-test.firebaseapp.com",
@@ -7,3 +10,27 @@ const config = {
     messagingSenderId: "228854286013",
     appId: "1:228854286013:web:36448ae9a36a682c",
 };
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+
+    }
+    // auth API from Firebase
+    doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email,password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+}
+
+
+export default Firebase;
