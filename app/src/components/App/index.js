eb878489c4268b942c7b62fde8c2a3393f27a6ec
Summer 19 <summer19@iMac-3.local>
2019-06-24 17:02:45 -0700

    added Session handling

--- app/src/components/App/index.js
+++ app/src/components/App/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
     BrowserRouter as Router, 
     Route,
@@ -14,51 +14,27 @@ import CalendarPage from '../Calendar';
 import FriendsPage from '../Friends';
 
 import * as ROUTES from '../../constants/routes';
-import { withFirebase } from '../Firebase';
+import { withAuthentication } from '../Session';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            authUser: null,
-        };
-    }
+const App = () => (
+    <Router>
+        <div>
+            <Navigation />
 
-    componentDidMount() {
-        this.listener = this.props.firebase.auth.onAuthStateChanged(
-            authUser => {
-                authUser
-                ? this.setState({ authUser })
-                : this.setState({ authUser: null });
-            },
-        );
-    }
+            <hr />
 
-    componentWillUnmount() {
-        this.listener();
-    }
+            <Route exact path={ROUTES.LANDING} component={LandingPage} />
+            <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+            <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+            <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+            <Route path={ROUTES.HOME} component={HomePage} />
+            <Route path={ROUTES.CALENDAR} component={CalendarPage} />
+            <Route path={ROUTES.FRIENDS} component={FriendsPage} />
+        </div>
+    </Router>
+);
 
-    render() {
-        return(
-            <Router>
-                <div>
-                    <Navigation authUser={this.state.authUser} />
-
-                    <hr />
-
-                    <Route exact path={ROUTES.LANDING} component={LandingPage} />
-                    <Route path={ROUTES.SIGN_IN} component={SignInPage} />
-                    <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
-                    <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
-                    <Route path={ROUTES.HOME} component={HomePage} />
-                    <Route path={ROUTES.CALENDAR} component={CalendarPage} />
-                    <Route path={ROUTES.FRIENDS} component={FriendsPage} />
-                </div>
-            </Router>
-        );
-    }
-}
 // Navigation this makes it that the Navigation page stays regardless of the route
 // Things below <hr /> shows the component that matches the path of the route
-export default withFirebase(App);
+export default withAuthentication(App);
